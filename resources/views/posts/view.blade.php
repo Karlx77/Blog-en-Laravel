ad1f24545e94b01ad9ac2ab73278d940ff0eb326
AssielMadai <leisamada@gmail.com>
2020-06-01 22:23:04 +0000

    Arreglando vista ded Blog

--- resources/views/posts/view.blade.php
+++ resources/views/posts/view.blade.php
@@ -15,38 +15,40 @@
                     <div class="card-header">Post View</div>
                     <div class="card-body">
                         <div class="col-md-12">
-                            <div class="card align-content-center col-md-6">
-                                <div class="card-header">
-                                    @if(count($posts)>0)
-                                        @foreach($posts->all() as $post)
-                                        <h3>{{$post->name}}</h3>
-                                </div>
-                                <div class="card-body">
-                                            <img src="{{ $post->post_image }}" alt="" width="450" height="450">
-                                </div>
-                                <div class="card-footer ">
-                                   <h4>{{ $post->post_title }}</h4>
-                                    <p>{{$post->post_body}}</p>
-                                    <ul class="nav nav-pills">
-                                        <li role="presentation">
-                                            <a class="btn btn-link" href="">
-                                                <img src="{{url('images/like.png')}}" alt="" style="height:35px;width:35px; ">
-                                            </a>
-                                        </li>
-                                        <li role="presentation">
-                                            <a class="btn btn-link" href="">
-                                                <img src="{{url('images/message.png')}}" alt="" style="height:35px;width:35px; ">
-                                            </a>
-                                        </li>
-                                    </ul>
-{{--                                    <span>{{$disCtr}} me gusta</span>--}}
-                                </div>
-                                @endforeach
-                                @else
-                                    <p>No Post Available</p>
-                                @endif
+                            <div class="row row-cols-1 row-cols-md-1 ">
+                                @if(count($posts)>0)
+                                    @foreach($posts->all() as $post)
+                                        <div class="card align-content-center col-md-6">
+                                            <div class="card-header ">
+                                                <h3>{{$post->name}}</h3>
+                                            </div>
+                                            <div class="card-body">
+                                                <img src="{{ $post->post_image }}" alt="" width="350" height="350">
+                                            </div>
+                                            <div class="card-footer ">
+                                                <h4>{{ $post->post_title }}</h4>
+                                                <p>{{$post->post_body}}</p>
+                                                <ul class="nav nav-pills">
+                                                    <li role="presentation">
+                                                        <a class="btn btn-link" href="">
+                                                            <img src="{{url('images/like.png')}}" alt="" style="height:35px;width:35px; ">
+                                                        </a>
+                                                    </li>
+                                                    <li role="presentation">
+                                                        <a class="btn btn-link" href="">
+                                                            <img src="{{url('images/message.png')}}" alt="" style="height:35px;width:35px; ">
+                                                        </a>
+                                                    </li>
+                                                </ul>
+                                                {{--                                    <span>{{$disCtr}} me gusta</span>--}}
+                                            </div>
+                                        </div>
+                            </div><br>
+                            @endforeach
+                            @else
+                                <p>No Post Available</p>
+                            @endif
                             </div>
-
                         </div>
                     </div>
                 </div>
