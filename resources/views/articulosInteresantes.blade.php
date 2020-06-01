6be804f2656d92e501102f550990f0157d9f42cc
AssielMadai <leisamada@gmail.com>
2020-06-01 20:57:51 +0000

    Arreglando Cooper Principal

---
+++ resources/views/articulosInteresantes.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.appCooper')
+
+@section('content')
+<div id ="pagina" class="container"><br>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Bolivia Segura</h5>
+                    <p class="card-text">En este sitio web se puede encontrar información actualizada sobre la enfermedad, recomendaciones dirigidas a los distintos sectores de la población y las diferentes medidas para atender la pandemia y sus efectos.</p>
+                    <a href="https://www.boliviasegura.gob.bo" class="btn btn-primary">Ir a la Pagina</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">OMS</h5>
+                    <p class="card-text">Anuncio Oficial desde la Organización Mundial de la Salud sobre la COVID-19. Síntomas, Prevención, Advertencia de Viaje y Preguntas Frecuentes sobre la COVID-19. Cómo protegerse. Orientaciones Técnicas. Consejos e información. Preguntas y Respuestas.</p>
+                    <a href="https://www.who.int/es/emergencies/diseases/novel-coronavirus-2019?gclid=CjwKCAjw2a32BRBXEiwAUcugiIPm1sP21_rP1iyaBq1niVQgMnl1f0yjrW0L_xPTGr0S_XMmzb2A_hoCKT8QAvD_BwE" class="btn btn-primary">Ir a la pagina</a>
+                </div>
+            </div>
+        </div>
+    </div><br>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Verificar Noticias Fake</h5>
+                    <p class="card-text">Bolivia Verifica es un medio digital sin fines de lucro ni tendencia política que se dedica a la verificación de noticias falsas (fake news) y del discurso público para luchar contra la desinformación y mejorar la participación democrática.</p>
+                    <a href="https://boliviaverifica.bo/" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
