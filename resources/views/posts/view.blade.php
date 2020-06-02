19ca83d13485ff5705100c37e22a4f5328c05736
Karlx77 <katniss_12_ja@outlook.es>
2020-06-01 21:17:18 -0400

    arreglando view

--- resources/views/posts/view.blade.php
+++ resources/views/posts/view.blade.php
@@ -13,29 +13,39 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">Post View</div>
-                    <div class="card-body row">
-                        <div class="col-md-8">
-                            @if(count($posts)>0)
-                                @foreach($posts->all() as $post)
-                                    <h4>{{ $post->post_title }}</h4>
-                                    <img src="{{ $post->post_image }}" alt="" width="450" height="450">
+                    <div class="card-body">
+                        <div class="col-md-12">
+                            <div class="card align-content-center col-md-6">
+                                <div class="card-body">
+
+                                </div>
+                                <div class="card-body">
+                                    @if(count($posts)>0)
+                                        @foreach($posts->all() as $post)
+                                            <img src="{{ $post->post_image }}" alt="" width="450" height="450">
+                                </div>
+                                <div class="card-body ">
+                                   <h4>{{ $post->post_title }}</h4>
                                     <p>{{$post->post_body}}</p>
                                     <ul class="nav nav-pills">
                                         <li role="presentation">
                                             <a class="btn btn-link" href="{{url('dislike',$post)}}">
-                                                <span><img src="{{url('images/like.png')}}" alt="" style="height:35px;width:35px; ">({{$disCtr}})</span>
+                                                <img src="{{url('images/like.png')}}" alt="" style="height:35px;width:35px; ">
                                             </a>
                                         </li>
                                         <li role="presentation">
                                             <a class="btn btn-link" href="">
-                                                <span class=" fa fa-comment"> Comment()</span>
+                                                <img src="{{url('images/message.png')}}" alt="" style="height:35px;width:35px; ">
                                             </a>
                                         </li>
                                     </ul>
-                                @endforeach
-                            @else
-                                <p>No Post Available</p>
-                            @endif
+                                    <span>{{$disCtr}} me gusta</span>
+                                    @endforeach
+                                    @else
+                                        <p>No Post Available</p>
+                                    @endif
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
