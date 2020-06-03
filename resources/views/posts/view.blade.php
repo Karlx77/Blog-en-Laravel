808ba59ddd2892b51651a359522a5dd466ca38f4
AssielMadai <leisamada@gmail.com>
2020-06-03 17:28:44 +0000

    Vista Principal

--- resources/views/posts/view.blade.php
+++ resources/views/posts/view.blade.php
@@ -14,94 +14,39 @@
                 <div class="card">
                     <div class="card-header">Post View</div>
                     <div class="card-body">
-                        <div class="col-md-12">
-                            <div class="row row-cols-1 row-cols-md-1 ">
-                                @if(count($posts)>0)
-                                    @foreach($posts->all() as $post)
-{{--                                        <div class="col-md-2"></div>--}}
-                                        <div class="card align-content-center col-md-8">
-                                            <div class="card-header ">
-                                                <h3>{{$post->name}}</h3>
-                                            </div>
-                                            <div class="card-body">
-                                                <img src="{{ $post->post_image }}" alt="" width="350" height="350">
-                                            </div>
-                                                <h4>{{ $post->post_title }}</h4>
-                                                <p>{{$post->post_body}}</p>
-                                                <ul class="nav nav-pills">
-                                                <li role="presentation">
-                                            <a class="btn btn-link" href="{{route('dislike',$post->idPost)}}">
-                                                <img src="{{url('images/like.png')}}" alt="" style="height:35px;width:35px; ">
-                                            </a>
-                                        </li>
-
-                                                    <li role="presentation">
-                                                        <a class="btn btn-link" href="{{route('dislike',$post->pos)}}">
-                                                            <img src="{{url('images/like.png')}}" alt="" style="height:35px;width:35px; ">
-                                                        </a>
-                                                    </li>
-                                                    <li role="presentation">
-                                                        <a class="btn btn-link" href="{{route('verMasComentarios',$post->idPost)}}">
-                                                            <img src="{{url('images/message.png')}}" alt="" style="height:35px;width:35px; ">
-                                                        </a>
-                                                    </li>
-                                                </ul>
-                                                   <span>{{$disCtr}} me gusta</span>
-                                            </div>
-                                                {{--                                    <span>{{$disCtr}} me gusta</span>--}}
-                                            <form action="" method="post">
-                                                {{--                                                    <label for="">Comentar</label>--}}
-                                                <textarea rows="2" type="text"></textarea>
-                                                <a class="btn btn-link" href="">
-                                                    <button type="submit" class="btn btn-primary">Enviar</button>
-                                                </a>
-                                            </form>
-                                        </div>
-                            </div><br>
-                            @endforeach
-                            @else
-                                <p>No Post Available</p>
-                            @endif
                         @if(count($posts)>0)
                             @foreach($posts->all() as $post)
-
-                            <div class="card mb-3" style="max-width: 740px;">
-                            <div class="row no-gutters">
-                                <div class="col-md-4">
-                                    <img src="{{ $post->post_image }}" alt="" width="230" height="210">
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="card-body">
-                                        <h5 class="card-title">{{$post->name}}</h5>
-                                        <h4>{{ $post->post_title }}</h4>
-                                        <p class="card-text">{{$post->post_body}}</p>
-                                        <p class="card-text"><small class="text-muted">
-                                                <div class="row">
-                                                    <ul class="nav nav-pills">
-                                                        <li role="presentation">
-                                                            <a class="btn btn-link" href="{{route('dislike',$post->pos)}}">
-                                                                <img src="{{url('images/like.png')}}" alt="" style="height:35px;width:35px; ">
-                                                            </a>
-                                                        </li>
-                                                        <li role="presentation">
-                                                            <a class="btn btn-link" href="">
-                                                                <img src="{{url('images/message.png')}}" alt="" style="height:35px;width:35px; ">
-                                                            </a>
-                                                        </li>
-                                                    </ul>
-{{--                                                    <form action="" method="post">--}}
-{{--                                                        <label for="">Comentar</label>--}}
-{{--                                                        <textarea rows="2" type="text"></textarea>--}}
-{{--                                                        <a class="btn btn-link" href="">--}}
-{{--                                                            <button type="submit" class="btn btn-primary">Enviar</button>--}}
-{{--                                                        </a>--}}
-{{--                                                    </form>--}}
-                                                </div>
-                                            </small></p>
+                                <div class="card mb-3" style="max-width: 740px;">
+                                    <div class="row no-gutters">
+                                        <div class="col-md-4">
+                                            <img src="{{ $post->post_image }}" alt="" width="230" height="210">
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="card-body">
+                                                <h5 class="card-title">{{$post->name}}</h5>
+                                                <h4>{{ $post->post_title }}</h4>
+                                                <p class="card-text">{{$post->post_body}}</p>
+                                                <p class="card-text"><small class="text-muted">
+                                                        <div class="row">
+                                                            <ul class="nav nav-pills">
+                                                                <li role="presentation">
+                                                                    <a class="btn btn-link" href="{{route('dislike',$post->idPost)}}">
+                                                                        <img src="{{url('images/like.png')}}" alt="" style="height:35px;width:35px; ">
+                                                                    </a>
+                                                                </li>
+                                                                <li role="presentation">
+                                                                    <a class="btn btn-link" href="{{route('verMasComentarios',$post->idPost)}}">
+                                                                        <img src="{{url('images/message.png')}}" alt="" style="height:35px;width:35px; ">
+                                                                    </a>
+                                                                </li>
+                                                            </ul>
+                                                            <span>{{$disCtr}} me gusta</span>
+                                                        </div>
+                                                    </small></p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
                             @endforeach
                         @else
                             <p>No Post Available</p>
